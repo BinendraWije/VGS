2b281b4876655baad05d957ad809bd77d8b2c0f9
BinendraWije <124019559+BinendraWije@users.noreply.github.com>
2023-09-11 23:13:20 +0530

    Update features.tsx

--- components/features.tsx
+++ components/features.tsx
@@ -26,7 +26,7 @@ function rightbutton(){
   
   const [posts, setPosts] = useState([])
  useEffect(()=>{
-fetch('http://localhost:3306/posts')
+fetch('http://16.170.240.192:3306/posts')
 .then(res => res.json())
 .then(data => setPosts(data))
 .catch(err => console.log(err));
