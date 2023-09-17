bff65beb76babd73691898f21cfe12144a56ec21
Billy <kingcool001@gmail.com>
2023-09-17 17:01:22 +0530

    added bcyrptjs

--- Adminpanel/src/partials/dashboard/UserDashboard.jsx
+++ Adminpanel/src/partials/dashboard/UserDashboard.jsx
@@ -10,8 +10,8 @@ import Image03 from '../../images/user-36-07.jpg';
 import Image04 from '../../images/user-36-08.jpg';
 import Image05 from '../../images/user-36-09.jpg';
 import axios from '../../utils/axios' 
+import bcrypt from 'bcryptjs';
 
-var bcrypt = require('bcryptjs');
 
 // ----------------------------------------------------------------------
 // -------Regex constants and URLS-------------------------------------------------
