a05c34a426e5becf11ba94077e4afbfee95b91a5
Billy <kingcool001@gmail.com>
2023-09-19 17:22:47 +0530

    restructuring backend

--- Backend/Routes/createUser.js
+++ Backend/Routes/createUser.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import { userSchema } from '../Backend/Model/register_user_schema.js';
-import {validateRequestSchema} from '../Backend/Middleware/validate_schema_requests.ts';
+import { userSchema } from '../Model/register_user_schema.js';
+import {validateRequestSchema} from '../Middleware/validate_schema_requests.js';
 import {body,validationResult} from 'express-validator';
 
 
