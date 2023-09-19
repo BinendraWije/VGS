1ed1c4f708683ada8702234b96fec9e5a17f26c2
Billy <kingcool001@gmail.com>
2023-09-19 21:49:11 +0530

    restructuring backend

--- Backend/database.js
+++ Backend/database.js
@@ -11,11 +11,12 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    port: process.env.MYSQL_PORT,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
+    host: "16.170.240.192",
+    port: "3306",
+    user: "main",
+    password: "Newpass11",
+    database: "vgsdb"
+    
 })
 db.connect(function(err){
    if(err){
