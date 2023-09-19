a5bee056004db6e616d16bc7e27d9d72138c4662
Billy <kingcool001@gmail.com>
2023-09-19 18:31:42 +0530

    restructuring backend

--- Backend/database.js
+++ Backend/database.js
@@ -4,17 +4,18 @@ const createUserRouter = require('./Routes/createUser.js');
 const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-    host: "16.170.240.192", 
-    port: "3306",
-    user: "main",
-    password: "Newpass11",
-    database: "vgsdb"
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_HOST
 })
 db.connect(function(err){
    if(err){
