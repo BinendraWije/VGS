add3bbb009d9ce0dd9e9104090ab89464a6e79ee
Billy <kingcool001@gmail.com>
2023-09-19 17:47:17 +0530

    restructuring backend

--- Backend/Middleware/validate_schema_requests.js
+++ Backend/Middleware/validate_schema_requests.js
@@ -1,8 +1,8 @@
-// import { validationResult } from "express-validator";
-const expressvalidator = require('express-validator'); 
+import { validationResult } from "express-validator";
+
 
 export function validateRequestSchema(req,res,next){
-    const errors = expressvalidator.validationResult(req);
+    const errors = validationResult(req);
     if(!errors.isEmpty()){
         return res.status(400).json({errors: errors.array()});
     }
