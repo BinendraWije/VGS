0e3d5259d52c0b7f2ef4ba21a25e6152f0e48613
Billy <kingcool001@gmail.com>
2023-09-20 23:50:15 +0530

    restructuring backend

--- Backend/Routes/createUser.js
+++ Backend/Routes/createUser.js
@@ -9,25 +9,28 @@ const findDuplicatesquery = "SELECT * FROM vgsdb.users WHERE `user_name` = ?";
 const duplicateSearch = db.query(findDuplicatesquery,[req.body.user_name], (err,data)=>{
     if(err) return res.json(err);
     console.log(data);      
-    if(data) return true;
-})
-console.log(duplicateSearch);
-if(duplicateSearch){
-     return res.sendStatus(409)
-      //conflict
-}else{
-const q = "INSERT INTO vgsdb.users (`user_name`,`user_pwd`,`user_role`) VALUES(?)";
-const values = [
-    req.body.user_name,
-    req.body.user_pwd,
-    req.body.user_role,
-    ]
-db.query(q,[values], (err,data)=>{
-        if(err) return res.json(err);      
-        return res.json("user successfully created");
+    if(data){
+        return res.sendStatus(409)      //conflict
 
-    })
-}
+    }else{
+
+        const q = "INSERT INTO vgsdb.users (`user_name`,`user_pwd`,`user_role`) VALUES(?)";
+        const values = [
+            req.body.user_name,
+            req.body.user_pwd,
+            req.body.user_role,
+            ]
+        db.query(q,[values], (err,data)=>{
+                if(err) return res.json(err);      
+                return res.json("user successfully created");
+        
+            })
+    };
+})
+
+     
+
+
 })
 
 module.exports =  { createUserRouter } ;
