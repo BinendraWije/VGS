44c526e51917370e772ac741c85a2a095758cda8
Billy <kingcool001@gmail.com>
2023-09-20 21:48:10 +0530

    restructuring backend

--- Backend/Routes/createUser.js
+++ Backend/Routes/createUser.js
@@ -5,7 +5,14 @@ const createUserRouter = express.Router();
 createUserRouter.post('/createuser',(req,res)=>{
     console.log('heere in th createroute');
 if(!req.body.user_name || !req.body.user_pwd)return res.status(400).json({'message':'Username and password are required.'});
+const findDuplicatesquery = "SELECT * FROM vgsdb.users WHERE `user_name` = ?";
+const duplicateSearch = db.query(findDuplicatesquery,req.body.user_name, (err,data)=>{
+    if(err) return res.json(err);
+    console.log(data);      
+    if(data) return true;
+})
 
+if(duplicateSearch) return res.sendStatus(409) //conflict
 const q = "INSERT INTO vgsdb.users (`user_name`,`user_pwd`,`user_role`) VALUES(?)";
 const values = [
     req.body.user_name,
