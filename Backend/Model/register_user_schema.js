e2d39afb4d538964fa8d3ee1d43ee9ed1e177fcc
Billy <kingcool001@gmail.com>
2023-09-20 16:38:59 +0530

    restructuring backend

--- Backend/Model/register_user_schema.js
+++ Backend/Model/register_user_schema.js
@@ -1,11 +1,9 @@
 const {body, param, query} = require('express-validator');
 
 const user_schema = [
-
 body('user_name').exists({checkFalsy: true}),
 body('user_pwd').exists({checkFalsy: true}),
 body('user_role').exists({checkFalsy: true})
-
 ];
 
 module.exports =  { user_schema } ;
--- Backend/Routes/createUser.js
+++ Backend/Routes/createUser.js
@@ -1,13 +1,8 @@
 const express = require('express');
-const {validateRequestSchema} = require('../Middleware/validate_schema_requests.js');
-const { user_schema } = require('../Model/register_user_schema.js');
 const { db } = require('../Config/databaseconfig.js');
 
 const createUserRouter = express.Router();
-createUserRouter.post('/createuser',
-user_schema,
-validateRequestSchema,
-(req,res)=>{
+createUserRouter.post('/createuser',(req,res)=>{
     console.log('heere in th createroute');
 if(!req.body.user_name || !req.body.user_pwd)return res.status(400).json({'message':'Username and password are required.'});
 
