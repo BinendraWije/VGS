0c362a4e2160429898e774539588f5740a0bc6f2
Billy <kingcool001@gmail.com>
2023-09-21 10:56:17 +0530

    restructuring backend

--- Adminpanel/src/pages/UserPage.jsx
+++ Adminpanel/src/pages/UserPage.jsx
@@ -119,7 +119,7 @@ export default function UserPage() {
       if(!err?.response){
         setErrMsg('No server Response');
       } else if (err.response?.status === 409){
-        setErrMsg(err);
+        setErrMsg("bleeep bllooop");
       } else{
         setErrMsg('Registration Failed')
       }
