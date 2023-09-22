d588efed12b9ba3f98da97778728c45a33c1fa1b
Billy <kingcool001@gmail.com>
2023-09-22 23:52:21 +0530

    restructuring backend

--- Adminpanel/src/pages/Signin.jsx
+++ Adminpanel/src/pages/Signin.jsx
@@ -66,7 +66,7 @@ const handleSubmit = async (e)=>{
     setUser('')
     setPwd('')
     setSuccess(true);
-    <Navigate to ='/'/>
+    return <Navigate to ='/'/>
     
   } catch (err) {
     if(!err?.response){
