a663116bb089e4aed467ff1d69254655e81d538e
Billy <kingcool001@gmail.com>
2023-09-24 14:01:32 +0530

    restructuring backend

--- Adminpanel/src/main.jsx
+++ Adminpanel/src/main.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, BrowserRouter as Router } from 'react-router-dom';
 import ThemeProvider from './utils/ThemeContext';
 import App from './App';
 import { AuthProvider } from './context/AuthProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-    <AuthProvider>
+     <AuthProvider>
     <Router>
       <ThemeProvider>
-        <App />
+        <Routes>
+          <Route path='/*' element = {<App/>} />
+        </Routes>
       </ThemeProvider>
     </Router>
     </AuthProvider>
-    </BrowserRouter>
   </React.StrictMode>
 );
