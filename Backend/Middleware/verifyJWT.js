c4cf0ce72996d0d807191a45f701e31f95771e92
Billy <kingcool001@gmail.com>
2023-09-25 09:17:55 +0530

    restructuring backend

--- Backend/Middleware/verifyJWT.js
+++ Backend/Middleware/verifyJWT.js
@@ -3,7 +3,10 @@ require('dotenv').config();
 
 
 const verifyJWT = (req,res,next) =>{
+    const authers = req.headers['authorization']
+    console.log(authers);
     const authHeader = req.headers.authorization || req.headers.Authorization;
+    console.log(authHeader);
     if(!authHeader && !authHeader.startsWith('Bearer ')) return res.sendStatus(401); //Unauthorized
     const token = authHeader.split(' ')[1];
     jwt.verify(
