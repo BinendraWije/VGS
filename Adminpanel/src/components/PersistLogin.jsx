dcff85f6abcf4bf759f4e17615e2ec6cffb64b48
Billy <kingcool001@gmail.com>
2023-09-26 10:01:09 +0530

    restructuring backend

--- Adminpanel/src/components/PersistLogin.jsx
+++ Adminpanel/src/components/PersistLogin.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import useRefreshToken from '../hooks/useRefreshToken';
 import useAuth from "../hooks/useAuth";
 
-const PeristLogin = () => {
+const PersistLogin = () => {
     const[isLoading, setIsLoading] = useState(true);
     const refresh = useRefreshToken();
     const { auth } = useAuth();
