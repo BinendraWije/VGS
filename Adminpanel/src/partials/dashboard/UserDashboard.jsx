ef99728f0fa8aabc013ad20ca70444f877f04d6a
Billy <kingcool001@gmail.com>
2023-09-29 17:56:42 +0530

    restructuring backend

--- Adminpanel/src/partials/dashboard/UserDashboard.jsx
+++ Adminpanel/src/partials/dashboard/UserDashboard.jsx
@@ -152,8 +152,8 @@ function UserDashboard() {
   },[]);
 
 
-  const deleteHandler = async (e,user_name)=>{
-    e.preventDefault();
+  const deleteHandler = async (user_name)=>{
+    
     const user = user_name;
 try{
     await axios.post(DELETE_USER_URL,
@@ -303,7 +303,7 @@ try{
               </button>
             </li>
             <li>
-              <button className="font-medium text-sm text-rose-500 hover:text-rose-600 flex py-1 px-3" to="#0" onClick={deleteHandler(e,user.user_name)}>
+              <button className="font-medium text-sm text-rose-500 hover:text-rose-600 flex py-1 px-3" to="#0" onClick={deleteHandler(user.user_name)}>
                 Delete
               </button>
             </li>
