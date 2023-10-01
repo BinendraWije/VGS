aa9635df8a1cb6ebda8eacec0a82b878b87e4995
Billy <kingcool001@gmail.com>
2023-10-01 19:21:26 +0530

    restructuring backend

--- Adminpanel/src/partials/dashboard/ProductDashboard.jsx
+++ Adminpanel/src/partials/dashboard/ProductDashboard.jsx
@@ -49,7 +49,7 @@ function Productdashboard() {
   const productPriceRef = useRef();
   const productTypeRef = useRef();
   const errRef = useRef();
-  
+
   const [product,setProduct] = useState('');
   const [productDescription,setProductDescription] = useState('');
   const [productImage,setProductImage] = useState('');
@@ -239,6 +239,28 @@ const editSubmitHandler = async (e) =>{
   fetchpost();
 }
   
+///////////////// temp image preview setup ////////////////////////////
+  const [files, setFiles] = useState();
+  const [previews, setPreviews] = useState();
+
+  useEffect(() => {
+    if (!files) return;
+    let tmp = [];
+    for (let i = 0; i < files.length; i++) {
+      tmp.push(URL.createObjectURL(files[i]));
+    }
+    const objectUrls = tmp;
+    setPreviews(objectUrls);
+
+    // free memory
+    for (let i = 0; i < objectUrls.length; i++) {
+      return () => {
+        URL.revokeObjectURL(objectUrls[i]);
+      };
+    }
+  }, [files]);
+
+
 
   return (
     <>
@@ -270,8 +292,17 @@ const editSubmitHandler = async (e) =>{
             <div className="formholder min-w-full">
              <p ref={errRef} className={errMsg ? "errmsg":"offscreen"}>{errMsg}</p> 
              <form onSubmit={editMode? editSubmitHandler : submitHandler}>
+             <label className='mx-1' htmlFor="productname"> Product Images Upload : </label>
+              <input type="file" accept="image/jpg, image/jpeg, image/png"multiple onChange={(e) => { if (e.target.files && e.target.files.length > 0) { setFiles(e.target.files); } }} />
+              <div className="imageholdersection">
+              {previews && previews.map((pic) => {
+                return <img src={pic} />;
+              })}
+              </div>
                 <label className='mx-1' htmlFor="productname"> Product Name:
                 <input type="text" id="productname" ref={ProductRef} autoComplete='off' onChange={(e)=>setProduct(e.target.value)} required  onFocus={()=>setProductFocus(true)} onBlur={()=>setProductFocus(false)} /></label>
+
+
 
                 <label className='mx-1' htmlFor="productdescription"> Product description: 
                 <input type="text" id="productdescription" ref={productDescriptionRef} autoComplete='off' onChange={(e)=>setProductDescription(e.target.value)} required  onFocus={()=>setProductDescriptionFocus(true)} onBlur={()=>setProductDescriptionFocus(false)} /></label>
