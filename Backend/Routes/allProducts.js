fafe5106cab986110c8b9c1e9d88d70e19e66542
Billy <kingcool001@gmail.com>
2023-10-01 16:09:13 +0530

    restructuring backend

--- Backend/Routes/allProducts.js
+++ Backend/Routes/allProducts.js
@@ -5,18 +5,18 @@ const { verifyRoles } = require('../Middleware/verifyRoles.js');
 const getAllProductsRouter = express.Router();
 getAllProductsRouter.get('/products',(req,res)=>{
     const sql = "SELECT * FROM vgsdb.Products";
-const products =  db.query(sql,(err,data)=>{
+const products =  db.query(sql,async (err,data)=>{
         if(err) return res.json(err);
         return data;
     })
 
     const sql1 = "SELECT * FROM vgsdb.producttype";
-    const producttypes = db.query(sql1,(err,data1)=>{
+    const producttypes = db.query(sql1, async(err,data1)=>{
         if(err) return res.json(err);
         return data1;
     }) 
-    console.log(products);
-    console.log(producttypes);    
+    console.log(products[0]);
+    console.log(producttypes[0]);    
     return res.json({products, producttypes});
 })
 
