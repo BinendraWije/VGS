1cd810b5bd38bba3334498dc19eaf51f7e26d030
Billy <kingcool001@gmail.com>
2023-10-01 16:18:33 +0530

    restructuring backend

--- Backend/Routes/allProducts.js
+++ Backend/Routes/allProducts.js
@@ -7,17 +7,17 @@ getAllProductsRouter.get('/products',(req,res)=>{
     const sql = "SELECT * FROM vgsdb.Products";
 const products =  db.query(sql,async (err,data)=>{
         if(err) return res.json(err);
-        return data.Row;
+        return data;
     })
 
     const sql1 = "SELECT * FROM vgsdb.producttype";
     const producttypes = db.query(sql1, async(err,data1)=>{
         if(err) return res.json(err);
-        return data1.Row;
+        return data1;
     }) 
     console.log(products);
     console.log(producttypes);    
-    return res.json({products, producttypes});
+    return res.json(products, producttypes);
 })
 
 module.exports = {getAllProductsRouter};
