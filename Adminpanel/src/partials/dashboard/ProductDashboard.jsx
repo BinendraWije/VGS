33e763d6da76062fc657c950f824248a2dbe4946
Billy <kingcool001@gmail.com>
2023-10-01 11:49:29 +0530

    restructuring backend

--- Adminpanel/src/partials/dashboard/ProductDashboard.jsx
+++ Adminpanel/src/partials/dashboard/ProductDashboard.jsx
@@ -293,14 +293,14 @@ const editSubmitHandler = async (e) =>{
                 </label>
                
              {editMode ? 
-             <button className="btn bg-indigo-500 hover:bg-indigo-600 text-white mx-2" disabled={!validName && !validPwd}>
+             <button className="btn bg-indigo-500 hover:bg-indigo-600 text-white mx-2" >
                  <svg className="w-4 h-4 fill-current opacity-50 shrink-0" viewBox="0 0 16 16">
                      <path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
                  </svg>
                  <span className="hidden xs:block ml-2">Edit Product</span>
              </button>                
               : 
-              <button className="btn bg-indigo-500 hover:bg-indigo-600 text-white mx-2" disabled={!validName && !validPwd}>
+              <button className="btn bg-indigo-500 hover:bg-indigo-600 text-white mx-2">
               <svg className="w-4 h-4 fill-current opacity-50 shrink-0" viewBox="0 0 16 16">
                   <path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
               </svg>
