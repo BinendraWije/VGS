6ae4ffde2f311a77b1915693c3173d41adb33227
Billy <kingcool001@gmail.com>
2023-10-03 08:55:57 +0530

    restructuring backend

--- Adminpanel/src/partials/dashboard/ProductDashboard.jsx
+++ Adminpanel/src/partials/dashboard/ProductDashboard.jsx
@@ -351,7 +351,7 @@ const editSubmitHandler = async (e) =>{
                <img src={pic1preview} /> 
               :
               <img />
-            }  <label for="picchooser absolute" class="btn">Upload</label>
+            }  <label htmlFor="picchooser" className="btn absolute">Upload</label>
                <input  id="picchooser" className='hidden' type="file" accept="image/jpg, image/jpeg, image/png" onChange={(e) => { if (e.target.files) { setPic1(e.target.files); } }}/>
                </div>
                <div className="imageholder flex flex-col p-10">
