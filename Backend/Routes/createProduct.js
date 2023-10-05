511a4cb5580584148b8f1d2968c870e575fdf043
Billy <kingcool001@gmail.com>
2023-10-05 20:14:30 +0530

    restructuring backend

--- Backend/Routes/createProduct.js
+++ Backend/Routes/createProduct.js
@@ -15,7 +15,8 @@ async (req,res)=>{
 if(!req.body.Product_Name || !req.body.Product_Description || !req.body.Product_Price || !req.body.product_type_ID || !req.body.Product_Quantity)return res.status(400).json({'message':'Check if all the data was submitted properly.'});
 console.log(req.body);
 console.log(req.body.Product_Image_1);
-
+console.log(req.body.Product_Image_1[0]);
+console.log(req.body.Product_Image_1.File);
 // Checking if username exists
 const findDuplicatesquery = "SELECT * FROM vgsdb.Products WHERE `Product_Name` = ?";
 db.query(findDuplicatesquery,[req.body.Product_Name], async (err,results)=>{
