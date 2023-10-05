2fec8349502b076cd65d3675382ea5edeee182e6
Billy <kingcool001@gmail.com>
2023-10-05 06:39:39 +0530

    restructuring backend

--- Adminpanel/src/partials/dashboard/ProductDashboard.jsx
+++ Adminpanel/src/partials/dashboard/ProductDashboard.jsx
@@ -364,10 +364,10 @@ const editSubmitHandler = async (e) =>{
               <p>or Drag and Drop an image</p>
 
             }            
-              <label htmlFor="picchooser" className="picchooser btn absolute bg-indigo-500 text-white button">{pic1? "Replace" : "Click to Upload"}</label>
+              <label htmlFor="picchooser" className="picchooser absolute text-indigo-500 ">{pic1? "Replace" : "Click to Upload"}</label>
             {
               pic1? 
-              <button className="removeimage absolute top-30" onClick={()=>{setPic1(null); setPic1preview(null)}}>Remove</button>
+              <button className="removeimage absolute top-60" onClick={()=>{setPic1(null); setPic1preview(null)}}>Remove</button>
               :
               <></>
             }
