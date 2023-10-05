8de7cf4b2d7f7772c75f9d3c69cb361b881093f9
Billy <kingcool001@gmail.com>
2023-10-05 20:21:27 +0530

    restructuring backend

--- Backend/Routes/createProduct.js
+++ Backend/Routes/createProduct.js
@@ -14,9 +14,9 @@ async (req,res)=>{
     //validate if required data exists
 if(!req.body.Product_Name || !req.body.Product_Description || !req.body.Product_Price || !req.body.product_type_ID || !req.body.Product_Quantity)return res.status(400).json({'message':'Check if all the data was submitted properly.'});
 console.log(req.body);
-console.log(req.body.Product_Image_1);
-console.log(req.body.Product_Image_2);
-console.log(req.body.Product_Image_2[0]);
+console.log(req.files.Product_Image_1);
+console.log(req.files.Product_Image_2);
+
 // Checking if username exists
 const findDuplicatesquery = "SELECT * FROM vgsdb.Products WHERE `Product_Name` = ?";
 db.query(findDuplicatesquery,[req.body.Product_Name], async (err,results)=>{
@@ -28,7 +28,7 @@ db.query(findDuplicatesquery,[req.body.Product_Name], async (err,results)=>{
 })
 //saving the files in the folder
 if(req.body.Product_Image_1){ 
-const folderpath1 = path.join(__dirname,'images',req.body.Product_Name,req.body.Product_Image_1.name);
+const folderpath1 = path.join(__dirname,'images',req.body.Product_Name,req.files.Product_Image_1.name);
 req.body.Product_Image_1.mv(folderpath1,(err)=>{
     if(err) return res.sendStatus(500);
 })
