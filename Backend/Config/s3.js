778b66900d4066d129b82acfb12ede5c05c172bb
Billy <kingcool001@gmail.com>
2023-10-06 23:46:59 +0530

    restructuring backend

--- Backend/Config/s3.js
+++ Backend/Config/s3.js
@@ -1,5 +1,5 @@
-import { S3Client, PutObjectCommand, DeleteObjectCommand, GetObjectCommand } from "@aws-sdk/client-s3"
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner"
+const { S3Client, PutObjectCommand, DeleteObjectCommand, GetObjectCommand } = require("@aws-sdk/client-s3")
+const { getSignedUrl }= require("@aws-sdk/s3-request-presigner")
 
 require('dotenv').config();
 
