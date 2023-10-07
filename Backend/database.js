3bf805c4dd910d1c61e95f119cdd192b40d35d9d
Billy <kingcool001@gmail.com>
2023-10-07 18:03:28 +0530

    restructuring backend

--- Backend/database.js
+++ Backend/database.js
@@ -18,7 +18,7 @@ const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
 require('dotenv').config();
-import { db } from '../Config/databaseconfig.js';
+import { db } from './Config/databaseconfig.js';
 import corsOptions from './Config/CORSoptions.js';
 import credentials from './Middleware/credentials.js';
 import { handleRefreshTokenrouter } from './Routes/refreshtokenroute.js';
