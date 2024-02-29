e10d62347cfeb6ae4ab9ec360c0a707b5db78d62
Billy <kingcool001@gmail.com>
2024-02-29 13:18:41 +0530

    changing up the log out

--- Backend/Routes/Signin.js
+++ Backend/Routes/Signin.js
@@ -51,7 +51,7 @@ db.query(findDuplicatesquery,[user], async (err,results)=>{
                 }
               });
         
-        res.cookie('jwt', refreshToken, {httpOnly:true, sameSite:'Lax',  maxAge: 24 * 60 * 60 * 1000});
+        res.cookie('jwt', refreshToken, {httpOnly:true, secure:true, sameSite:'None',  maxAge: 24 * 60 * 60 * 1000});
         res.json({ user_role, accessToken }); 
     }else{
         res.sendStatus(401);
