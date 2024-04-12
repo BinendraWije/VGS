e4b8e691d197f52ff47b9fb98256c27eeb47317c
Billy <kingcool001@gmail.com>
2024-04-12 19:32:44 +0530

    checking user data

--- Adminpanel/src/pages/UserDatapage.jsx
+++ Adminpanel/src/pages/UserDatapage.jsx
@@ -1,16 +1,36 @@
-import { React, useContext, useDebugValue } from 'react'
+import { React, useContext, useDebugValue, useState } from 'react'
 import AuthContext from "../context/AuthProvider";
+
+import axios from '../utils/axios'
+
 
 const UserDatapage = () => {
     const { auth } = useContext(AuthContext);  
     console.log(auth)
+
+    const GET_PRODUCT_URL = '/myprofile/';
+
+    const [usera,setUsera] = useState('');
+
+    const getMyprofile =  async (e, Product_Name) => {
+    e.preventDefault();
+    // get the single product
+  try{
+    const  userresponse = await axios.get(GET_PRODUCT_URL + auth.user);
+    setUsera(userresponse)
+  }catch(err){
+    console.log(err)
+  }
+    }
+
   return (
     <div>
     <h1>UserDatapage</h1>
     <div>
-        <h2>user : {auth.user}</h2>
-        <h2>pwd : {auth.pwd}</h2>
-                
+        <h2>user :{usera}</h2>   
+        <button className="btn bg-indigo-500 hover:bg-indigo-600 text-white mx-2" onClick={()=>getMyprofile()}>
+                MY profile
+              </button>
       
     </div>
     </div>
---
+++ Backend/Routes/checkUserdata.js
@@ -0,0 +1,22 @@
+// These lines make "require" available
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
+const express = require('express');
+import { db } from '../Config/databaseconfig.js';
+import { verifyRoles } from '../Middleware/verifyRoles.js';
+import { verifyJWT } from "../Middleware/verifyJWT.js";
+
+export const getMyprofileRouter = express.Router();
+getMyprofileRouter.get('/myprofile/:username', verifyJWT, async (req,res)=>{
+
+// Checking if username exists
+const findDuplicatesquery = "SELECT * FROM vgsdb.users WHERE `user_name` = ?";
+db.query(findDuplicatesquery,[req.params.username], async (err,results)=>{
+    if(err) return res.json(err);
+    const user = results[0]; 
+   return res.json(user)
+})
+
+});
+
