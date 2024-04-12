1299969de98b30b7e448a543698783ab63d570e0
Billy <kingcool001@gmail.com>
2024-04-12 23:13:28 +0530

    checking user data

--- Backend/Middleware/verifyJWT.js
+++ Backend/Middleware/verifyJWT.js
@@ -10,13 +10,12 @@ export const verifyJWT = (req,res,next) =>{
     //const authHeader = req.headers.authorization || req.headers.Authorization || req.headers['authorization'] || req.headers['Authorization'] ;
     const authHeader = req.headers.cookie    
     if(!authHeader && !authHeader.startsWith('jwt ')) return res.sendStatus(401); //Unauthorized
-    console.log(authHeader);
-    const token = authHeader.split('jwt=')[1];
-    console.log(token);   
+    const token = authHeader.split('jwt=')[1];  
     jwt.verify(
         token,
         process.env.ACCESS_TOKEN_SECRET,
         (err,decoded)=>{
+            console.log(decoded);
             if(err) return res.sendStatus(403); //Forbidden invalid token 
             req.user_name = decoded.UserInfo.user_name;
             req.user_role = decoded.UserInfo.user_role;
