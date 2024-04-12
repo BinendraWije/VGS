54fc8c7b0722a53e8c0505acce4c7307d1a43f68
Billy <kingcool001@gmail.com>
2024-04-12 17:18:25 +0530

    checking user data

--- Adminpanel/src/pages/UserDatapage.jsx
+++ Adminpanel/src/pages/UserDatapage.jsx
@@ -2,13 +2,16 @@ import { React, useContext, useDebugValue } from 'react'
 import AuthContext from "../context/AuthProvider";
 
 const UserDatapage = () => {
-    const { auth } = useContext(AuthContext);
+    const { auth } = useContext(AuthContext);  
   return (
     <div>
     <h1>UserDatapage</h1>
     <div>
         <h2>user : {auth.user}</h2>
         <h2>pwd : {auth.pwd}</h2>
+        <h2>auth : {auth}</h2>
+        
+      
     </div>
     </div>
    
