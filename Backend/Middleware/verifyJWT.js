f4b3d8c2ca25cde2ec5d4f127ff1d6adfa3f912a
Billy <kingcool001@gmail.com>
2024-04-13 06:10:37 +0530

    checking user data

--- Backend/Middleware/verifyJWT.js
+++ Backend/Middleware/verifyJWT.js
@@ -8,10 +8,10 @@ require('dotenv').config();
 
 export const verifyJWT = (req,res,next) =>{
     //const authHeader = req.headers.authorization || req.headers.Authorization || req.headers['authorization'] || req.headers['Authorization'] ;
+    console.log(req.headers)
     const authHeader = req.headers.cookie    
     if(!authHeader && !authHeader.startsWith('jwt ')) return res.sendStatus(401); //Unauthorized
     const token = authHeader.split('jwt=')[1];
-    console.log(token);
     jwt.verify(
         token,
         process.env.ACCESS_TOKEN_SECRET,
