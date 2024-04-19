85a222b953d014f67a36aba0742235a6697772b8
Billy <kingcool001@gmail.com>
2024-04-19 20:00:16 +0530

    checking user data

--- Backend/Routes/checkUserdata.js
+++ Backend/Routes/checkUserdata.js
@@ -9,7 +9,7 @@ import { verifyJWT } from "../Middleware/verifyJWT.js";
 
 export const getMyprofileRouter = express.Router();
 getMyprofileRouter.get('/myprofile/:username', verifyJWT, async (req,res)=>{
-
+console.log("We're in the profile checker")
 // Checking if username exists
 const findDuplicatesquery = "SELECT * FROM vgsdb.users WHERE `user_name` = ?";
 db.query(findDuplicatesquery,[req.params.username], async (err,results)=>{
