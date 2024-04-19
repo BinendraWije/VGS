3ae3821d4ffef4e961f640f024ad60a8bc384d3a
Billy <kingcool001@gmail.com>
2024-04-19 19:51:18 +0530

    checking user data

--- Adminpanel/src/pages/UserDatapage.jsx
+++ Adminpanel/src/pages/UserDatapage.jsx
@@ -15,7 +15,7 @@ const UserDatapage = () => {
     e.preventDefault();
     // get the single product
   try{
-    const  userresponse = await axios.post(GET_PRODUCT_URL + auth.user,
+    const  userresponse = await axios.get(GET_PRODUCT_URL + auth.user,
       JSON.stringify({ 
       accessToken: auth.accessToken     
     }),{
