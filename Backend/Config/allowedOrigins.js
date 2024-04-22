aa99ca351cfc24579e54ba046eaea2affcc20fef
Billy <kingcool001@gmail.com>
2024-04-22 21:30:37 +0530

    checking user data

--- Backend/Config/allowedOrigins.js
+++ Backend/Config/allowedOrigins.js
@@ -2,7 +2,9 @@ export const allowedOrigins = [
     'http://13.49.145.29:3000',
     'http://13.49.145.29',
     'http://localhost:3000',
-    'http://13.49.145.29:3306'
+    'http://13.49.145.29:3306',
+    'http://ec2-13-49-145-29.eu-north-1.compute.amazonaws.com',
+    'http://ec2-13-49-145-29.eu-north-1.compute.amazonaws.com:3000'
 
     
     
