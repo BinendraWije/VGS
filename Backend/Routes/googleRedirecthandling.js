9b999636f56e26bbb3ad30a48f4fb68f2f2b870b
Billy <kingcool001@gmail.com>
2024-04-23 13:20:12 +0530

    checking user data

--- Backend/Routes/googleRedirecthandling.js
+++ Backend/Routes/googleRedirecthandling.js
@@ -16,7 +16,11 @@ async function getUserData(access_token){
     const response = await fetch(`https://www.googleapis.com/oauth2/v3/userinfo?access_token=${access_token}`);
     const data = await response.json();
     console.log('data', data);
+    return data
 }
+
+const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
+const PWD_REGEX = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
 
 export const googleRedirectRouter = express.Router();
 googleRedirectRouter.get('/oauth', async (req,res)=>{
@@ -36,57 +40,89 @@ try{
     console.log('Tokens acquired')
     const user= oAuth2Client.credentials;
     console.log('credentials', user);
-    await getUserData(user.access_token)
-}
+    const data = await getUserData(user.access_token)
+
+        // Checking if email exists
+        // username is the name of the user
+        const findDuplicatesquery = "SELECT * FROM vgsdb.users WHERE `user_name` = ?";
+        db.query(findDuplicatesquery,[data.name], async (err,results)=>{
+        if(err) return res.json(err);         
+        if(results.length !== 0 ){
+    
+        // evaluate password
+        const match = await bcrypt.compare(data.email + data.sub, results[0].user_pwd);
+        if(match){
+            // create JWT
+            // gettin the user role from the results
+            const user_role = results[0].user_role;
+
+            const accessToken = jwt.sign(
+                {"UserInfo": {
+                    "user_name": data.name,
+                    "user_role" : user_role
+                    }
+                },
+                process.env.ACCESS_TOKEN_SECRET,
+                {expiresIn: '10s'}
+            );
+
+            const refreshToken = jwt.sign(
+                {"user_name": data.name},
+                process.env.REFRESH_TOKEN_SECRET,
+                {expiresIn: '40s'}
+            );
+        //Updating the refresh token to the current user's profile in DB
+
+            const refreshtokenquery = "UPDATE vgsdb.users SET refresh_token = ? WHERE user_name = ?";
+      
+            db.query(refreshtokenquery,[refreshToken,data.name], async (err,data)=>{
+                    if(err){ return res.json(err);}else{                    
+                    }
+                });
+        
+            res.cookie('jwt', refreshToken, {httpOnly:true, sameSite:'Lax',  maxAge: 24 * 60 * 60 * 1000});
+            res.json({ user_role, accessToken }); 
+        }else{
+            res.sendStatus(401);
+        }
+    }else{
+        // Create a new user      
+        // Checking if username exists
+const findDuplicatesquery = "SELECT * FROM vgsdb.users WHERE `user_name` = ?";
+db.query(findDuplicatesquery,[data.name], async (err,results)=>{
+    if(err) return res.json(err);         
+    if(results.length > 0 ){
+        return res.sendStatus(409);    
+
+    }
+})
+
+const hash = await bcrypt.hash(data.email + data.sub,10);
+  // inserting into the database
+
+const q = "INSERT INTO vgsdb.users (`user_name`,`user_pwd`,`user_role`) VALUES(?)";
+const values = [
+      data.name,
+      hash,
+      "Admin",
+      ]
+db.query(q,[values], async (err,data)=>{
+          if(err) return res.json(err);      
+          return res.json("user successfully created");
+  
+      })
+
+
+    }
+    
+    })
+
+window.location.href = 'http://13.49.145.29:3000/dashboard';
+    }
 catch(err){
 res.json(err)
 }
-/* Checking if username exists
-const findDuplicatesquery = "SELECT * FROM vgsdb.users WHERE `user_name` = ?";
-db.query(findDuplicatesquery,[user], async (err,results)=>{
-    if(err) return res.json(err);         
-    if(results.length == 0 ) return res.sendStatus(401); //Unauthorized
-    
-    // evaluate password
-    const match = await bcrypt.compare(req.body.user_pwd, results[0].user_pwd);
-    if(match){
-        // create JWT
-        // gettin the user role from the results
-        const user_role = results[0].user_role;
 
-        const accessToken = jwt.sign(
-            {"UserInfo": {
-                "user_name": req.body.user_name,
-                "user_role" : user_role
-                }
-            },
-            process.env.ACCESS_TOKEN_SECRET,
-            {expiresIn: '10s'}
-        );
 
-        const refreshToken = jwt.sign(
-            {"user_name": req.body.user_name},
-            process.env.REFRESH_TOKEN_SECRET,
-            {expiresIn: '40s'}
-        );
-        //Updating the refresh token to the current user's profile in DB
-
-        const refreshtokenquery = "UPDATE vgsdb.users SET refresh_token = ? WHERE user_name = ?";
-      
-        db.query(refreshtokenquery,[refreshToken,req.body.user_name], async (err,data)=>{
-                  if(err){ return res.json(err);}else{                    
-                }
-              });
-        
-        res.cookie('jwt', refreshToken, {httpOnly:true, sameSite:'Lax',  maxAge: 24 * 60 * 60 * 1000});
-        res.json({ user_role, accessToken }); 
-    }else{
-        res.sendStatus(401);
-    }
-
-    
-})
-
-*/
 });
 
--- Backend/Routes/googleSignin.js
+++ Backend/Routes/googleSignin.js
@@ -32,52 +32,5 @@ const authorizeURL = oAuth2Client.generateAuthUrl({
 
 res.json({url:authorizeURL});
 
-/* Checking if username exists
-const findDuplicatesquery = "SELECT * FROM vgsdb.users WHERE `user_name` = ?";
-db.query(findDuplicatesquery,[user], async (err,results)=>{
-    if(err) return res.json(err);         
-    if(results.length == 0 ) return res.sendStatus(401); //Unauthorized
-    
-    // evaluate password
-    const match = await bcrypt.compare(req.body.user_pwd, results[0].user_pwd);
-    if(match){
-        // create JWT
-        // gettin the user role from the results
-        const user_role = results[0].user_role;
-
-        const accessToken = jwt.sign(
-            {"UserInfo": {
-                "user_name": req.body.user_name,
-                "user_role" : user_role
-                }
-            },
-            process.env.ACCESS_TOKEN_SECRET,
-            {expiresIn: '10s'}
-        );
-
-        const refreshToken = jwt.sign(
-            {"user_name": req.body.user_name},
-            process.env.REFRESH_TOKEN_SECRET,
-            {expiresIn: '40s'}
-        );
-        //Updating the refresh token to the current user's profile in DB
-
-        const refreshtokenquery = "UPDATE vgsdb.users SET refresh_token = ? WHERE user_name = ?";
-      
-        db.query(refreshtokenquery,[refreshToken,req.body.user_name], async (err,data)=>{
-                  if(err){ return res.json(err);}else{                    
-                }
-              });
-        
-        res.cookie('jwt', refreshToken, {httpOnly:true, sameSite:'Lax',  maxAge: 24 * 60 * 60 * 1000});
-        res.json({ user_role, accessToken }); 
-    }else{
-        res.sendStatus(401);
-    }
-
-    
-})
-
-*/
 });
 
