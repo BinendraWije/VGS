7c9a8e7842e9a52980bfac8d43b16ff77a00fcb5
Billy <kingcool001@gmail.com>
2024-04-23 07:16:51 +0530

    checking user data

--- Backend/Routes/googleRedirecthandling.js
+++ Backend/Routes/googleRedirecthandling.js
@@ -13,7 +13,7 @@ require('dotenv').config();
 
 async function getUserData(access_token){
     console.log("User Data is triggered")
-    const response = await fetch(`https://www.googleapis.com/oauth2/v3/userinfo?access_token${access_token}`);
+    const response = await fetch(`https://www.googleapis.com/oauth2/v3/userinfo?access_token=${access_token}`);
     const data = await response.json();
     console.log('data', data);
 }
