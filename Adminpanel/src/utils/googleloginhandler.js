4b8ebe742bc669b8e2da8be4b27fe5adf5566b22
Billy <kingcool001@gmail.com>
2024-04-24 15:23:05 +0530

    checking user data

--- Adminpanel/src/utils/googleloginhandler.js
+++ Adminpanel/src/utils/googleloginhandler.js
@@ -1,26 +1,26 @@
-import React from "react";
-import { OAuth2Client } from "google-auth-library";
-
-
 async function googleLoginHandler(){
+
+const rootUrl = "https://accounts.google.com/o/oauth2/v3/auth";
 
 const redirectURL ='http://ec2-13-49-145-29.eu-north-1.compute.amazonaws.com:3306/oauth';
 
 console.log(process.env.REACT_APP_GOOGLE_CLIENT_ID);
 
-const oAuth2Client = new OAuth2Client(
-process.env.REACT_APP_GOOGLE_CLIENT_ID,
-process.env.REACT_APP_GOOGLE_CLIENT_SECRET,
-redirectURL 
-);
+const options = {
+client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+redirect_uri:redirectURL,
+access_type: ' offline',
+response_type: 'code',
+prompt: 'consent',
+scope:'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile openid'
+};
 
-const authorizeURL = oAuth2Client.generateAuthUrl({
-    access_type:'offline',
-    scope:'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile openid',
-    prompt:'consent' 
-})
-console.log(authorizeURL);
-return authorizeURL;
+
+const qs = new URLSearchParams(options);
+
+console.log(qs);
+
+return `${rootUrl}?${qs.toString()}`;
 
 };
 
