b24d0ce7d7aa2ea3837c33b5fd906065801f0d3c
Billy <kingcool001@gmail.com>
2024-04-26 17:35:18 +0530

    checking user data

--- Backend/Routes/newgoogleredirecthandling.js
+++ Backend/Routes/newgoogleredirecthandling.js
@@ -28,21 +28,32 @@ newgoogleRedirectRouter.get('/oauth', async (req,res)=>{
             grant_type: 'authorization_code'
         };
         try{
-            console.log("User Data is triggered")
-            const response = await axios.post(url,qs.stringify(values),{headers:{'Content-Type':'application/x-www-form-urlencoded'}});
-            const{id_token, access_token} = response.data;
-            console.log(id_token,access_token);
+            console.log("Access and id token fetch is triggered")
+            const response1 = await axios.post(url,qs.stringify(values),{headers:{'Content-Type':'application/x-www-form-urlencoded'}});
+            const{id_token, access_token} = response1.data;     
+
+            // get the user with tokens
+            console.log("getting userdata section triggered")
+            const response2 = await axios.get(`https://www.googleapis.com/oauth2/v3/userinfo?access_token=${access_token}`,{
+                headers: {
+                    Authorization: `Bearer ${id_token}`
+                }
+            })
+            console.log(response2.data);   
+
+            //update the user data
+
+            //create access and refresh jwt tokens and set cookie
+
+
+
+
+
         }catch(err){
                 res.json(err);
         }
 
-        // get the user with tokens
-
-
-        //update the user data
-
-        //create access and refresh jwt tokens and set cookie
-
+        
 
 
 
