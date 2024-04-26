094889da398f7257da6119b8663669561e7fdce2
Billy <kingcool001@gmail.com>
2024-04-26 15:15:07 +0530

    checking user data

--- Adminpanel/src/utils/googleloginhandler.js
+++ Adminpanel/src/utils/googleloginhandler.js
@@ -8,7 +8,7 @@ const redirectURL ='http://ec2-13-49-145-29.eu-north-1.compute.amazonaws.com:330
 const options = {
 client_id: import.meta.env.VITE_GOOGLE_CLIENT_ID,
 redirect_uri:redirectURL,
-access_type: ' offline',
+access_type: 'offline',
 response_type: 'code',
 prompt: 'consent',
 scope:'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile openid'
