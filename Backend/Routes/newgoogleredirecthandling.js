d756ac06e111f75bce0d173c1c4addaab0991a48
Billy <kingcool001@gmail.com>
2024-04-26 19:20:24 +0530

    checking user data

--- Backend/Routes/newgoogleredirecthandling.js
+++ Backend/Routes/newgoogleredirecthandling.js
@@ -78,8 +78,8 @@ newgoogleRedirectRouter.get('/oauth', async (req,res)=>{
                         }
                     });                                          
               
-              //res.cookie('jwt', refreshToken, { domain:'13.49.145.29:3000', httpOnly:true, sameSite:'Lax', path:'/',maxAge: 24 * 60 * 60 * 1000})
-              res.cookie('jwt', refreshToken, {httpOnly:true, sameSite:'Lax',  maxAge: 24 * 60 * 60 * 1000});
+              res.cookie('jwt', refreshToken, { domain:'13.49.145.29:3000', httpOnly:true, sameSite:'Lax', path:'/dashboard',maxAge: 24 * 60 * 60 * 1000})
+              //res.cookie('jwt', refreshToken, {httpOnly:true, sameSite:'Lax',  maxAge: 24 * 60 * 60 * 1000});
               res.json({ user_role, accessToken });
               //res.redirect('http://13.49.145.29:3000'); 
             }else{
