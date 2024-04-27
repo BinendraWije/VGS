3342f7c0f675048f5927e00c295904f5fcdd74d0
Billy <kingcool001@gmail.com>
2024-04-27 14:33:01 +0530

    checking user data

--- Backend/Routes/googleSignin.js
+++ Backend/Routes/googleSignin.js
@@ -30,6 +30,6 @@ const authorizeURL = oAuth2Client.generateAuthUrl({
     prompt:'consent' 
 })
 console.groupEnd("this fired bitch")
-res.jes({url:authorizeURL});
+res.json({url:authorizeURL});
 });
 
