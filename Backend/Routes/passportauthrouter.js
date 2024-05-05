d54700506fe99067eef84a9f9b32491adb57f013
Billy <kingcool001@gmail.com>
2024-05-05 23:01:12 +0530

    checking user data

--- Backend/Routes/passportauthrouter.js
+++ Backend/Routes/passportauthrouter.js
@@ -10,5 +10,28 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const passport = require('passport');
 
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://ec2-13-49-145-29.eu-north-1.compute.amazonaws.com:3306/oauth"
+},
+function(accessToken,refreshToken,profile, done){
+  done(null,profile)
+  // create jwttokens and save users 
+  console.log(profile)
+}
+
+));
+passport.serializeUser((user,done)=>{
+    done(null,user)
+})
+
+passport.deserializeUser((user,done)=>{
+    done(null,user)
+});
+
+
 export const passportauthRedirectrouter = express.Router();
 passportauthRedirectrouter.get('/passportgoogle', passport.authenticate('google', { scope: [ 'profile' ] }));
