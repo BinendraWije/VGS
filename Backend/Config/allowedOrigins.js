33e6504212101a45daa28093603de58abf768453
Billy <kingcool001@gmail.com>
2024-05-09 16:02:49 +0530

    checking user data

--- Backend/Config/allowedOrigins.js
+++ Backend/Config/allowedOrigins.js
@@ -4,7 +4,9 @@ export const allowedOrigins = [
     'http://localhost:3000',
     'http://13.49.145.29:3306',
     'http://ec2-13-49-145-29.eu-north-1.compute.amazonaws.com',
-    'http://ec2-13-49-145-29.eu-north-1.compute.amazonaws.com:3000'
+    'http://ec2-13-49-145-29.eu-north-1.compute.amazonaws.com:3000',
+    'http://www.hiawathaart.art',
+    'https://www.hiawathaart.art'
 
     
     
