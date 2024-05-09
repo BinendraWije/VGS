8db529908be939d9e8a7b4fb87aa26b4476819ba
Billy <kingcool001@gmail.com>
2024-05-09 22:19:22 +0530

    checking user data

--- Adminpanel/vite.config.js
+++ Adminpanel/vite.config.js
@@ -28,8 +28,7 @@ export default defineConfig({
     }
   },
   server:{
-    host: true,
-    port: 3000,
-    origin :'http://13.49.145.29:3000'
+    host: 'hiawathaart.art',
+    port: 80		
   } 
 })
