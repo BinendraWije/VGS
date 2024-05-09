1805bae060c9116168fbd5ddbe2f395a318a04f2
Billy <kingcool001@gmail.com>
2024-05-09 22:03:57 +0530

    checking user data

--- Adminpanel/vite.config.js
+++ Adminpanel/vite.config.js
@@ -29,7 +29,7 @@ export default defineConfig({
   },
   server:{
     host: true,
-    port: 3000,
-    origin :'http://13.49.145.29:3000'
+    port: 80,
+    origin :'http://13.49.145.29:80'
   } 
 })
